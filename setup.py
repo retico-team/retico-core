744215849278f151e48b041147e41017d1b085be
Thilo Michael <thilo.michael@tu-berlin.de>
2022-08-12 16:38:07 +0200

    Changed setup files for uploat to pypi

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ Setup script.
 
 Use this script to install the core of the retico simulation framework. Usage:
     $ python3 setup.py install
-The run the simulation:
-    $ retico [-h]
+
+Author: Thilo Michael (uhlomuhlo@gmail.com)
 """
 
 try:
@@ -16,16 +16,31 @@ except ImportError:
 
 import retico_core
 
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
 config = {
     "description": "A framework for real time incremental dialogue processing.",
+    "long_description": long_description,
+    "long_description_content_type": "text/markdown",
     "author": "Thilo Michael",
-    "url": "??",
-    "download_url": "??",
-    "author_email": "thilo.michael@tu-berlin.de",
+    "author_email": "uhlomuhlo@gmail.com",
+    "url": "https://github.com/retico-team/retico-core",
+    "download_url": "https://github.com/retico-team/retico-core",
     "version": retico_core.__version__,
+    "python_requires": ">=3.6, <4",
+    "keywords": "retico, framework, incremental, dialogue, dialog",
     "install_requires": ["pyaudio~=0.2.11"],
     "packages": find_packages(),
     "name": "retico-core",
+    "classifiers": [
+        "Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+    ],
 }
 
 setup(**config)
